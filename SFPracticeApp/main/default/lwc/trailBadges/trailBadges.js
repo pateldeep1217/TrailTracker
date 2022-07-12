d35af85a0e3685fd2f2b5feb103dcb233234d0a0
Tobi Diaz <tobiasd27@gmail.com>
2022-07-11 21:40:34 -0400

    Trail Badge LWC Updated

--- SFPracticeApp/main/default/lwc/trailBadges/trailBadges.js
+++ SFPracticeApp/main/default/lwc/trailBadges/trailBadges.js
@@ -3,11 +3,49 @@ import {getRecord} from 'lightning/uiRecordApi';
 
 import ID from '@salesforce/schema/Resources__c.User__c';
 import userBadges from '@salesforce/apex/UserBadgesController.getUserBadge';
-export default class TrailBadges extends LightningElement {
-    
-    Id
-    @api recordId
+export default class TrailBadges extends LightningElement 
+{
+    //ID
+    //@api recordId
+    @track users;
+    @track user;
 
+    @wire(userBadges)
+    wiredUserBadges({error, data})
+    {
+        if(data)
+        {
+            this.users = data;
+
+            for (const key in this.users)
+            {
+                //const maybestring = JSON.stringify(this.users[key].ID);
+                
+                if(this.users[key].ID == "0058Z000007y6sDQAQ")
+                {
+                    //this.user = this.users[key];
+                    //console.log(this.users[key]);
+                    console.log('This makes no fucking sense');
+                    
+                }
+                console.log('This makes no fucking sense INTENSIFIES');
+
+                
+                console.log(this.users[key].Id);
+                console.log(this.users[key].Badges);
+            }
+
+            //console.log(this.users[0].Id);
+        }
+
+        else if (error)
+        {
+            console.log(error);
+        }
+    }
+}
+
+/*
     @wire(getRecord, {recordId:'$recordId', fields:[ID]})
     resourceHandler({data}){
         if(data){
@@ -16,41 +54,18 @@ export default class TrailBadges extends LightningElement {
       
         }
     }
-
     
-
-
     @wire(userBadges)
     badges(data){
         if(data){
-     
-    
-        
-                console.log('data =  ' + JSON.stringify(data.data));
-                
+                console.log('data =  ' + JSON.stringify(data.data)); 
                 console.log('data without stringify =  ' + data.data);
-
-           
-        
-       
         }   
     }
-
-    
-
-}
-
-
-
-
-
-
-    
+    */
 // for(let i in this.result){
 //     console.log('result loop = ' + this.result[i]);
 //     for(let j in this.result){
 //         console.log('result loop 2x = ' +  JSON.stringify(this.result[i][j]))
-
-      
 //     }
 // }
