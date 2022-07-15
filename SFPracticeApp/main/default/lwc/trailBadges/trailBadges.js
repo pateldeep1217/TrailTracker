d8c6dd93c322c6eefde1bd897852b91ebd998874
Tobi Diaz <tobiasd27@gmail.com>
2022-07-14 20:12:18 -0400

    No real changes made, just some reformatting. Everything should work as intended.

--- SFPracticeApp/main/default/lwc/trailBadges/trailBadges.js
+++ SFPracticeApp/main/default/lwc/trailBadges/trailBadges.js
@@ -1,38 +1,33 @@
 import { LightningElement, wire,api,track } from 'lwc';
 import {getRecord} from 'lightning/uiRecordApi';
-
 import ID from '@salesforce/schema/Resources__c.User__c';
 import userBadges from '@salesforce/apex/UserBadgesController.getUserBadge';
 import userBadgesInfo from '@salesforce/apex/UserBadgesController.getUserBadgesInfo';
 
-
-const COLUMNS = [
+const COLUMNS = 
+[
     {label: 'Badge Name', fieldName: 'trailheadapp__Badge__r.Name', hideDefaultActions:true, type: 'text'}
     //{label: 'Badge Icon', fieldName: 'trailheadapp__Badge_Icon__c', hideDefaultActions:true, type: 'image'}
 ]
 
 export default class TrailBadges extends LightningElement 
 {
-
-
-    @track columns = COLUMNS;
-    ID
-    @api recordId
+    @api recordId;
+    @api badgesInfo;
     @track users;
-  
-    userId
-    @api badgesInfo
-    count
-    
-
+    @track columns = COLUMNS;
+    ID;
+    userId;
+    count;
     
     @wire(getRecord, {recordId:'$recordId', fields:[ID]})
-    resourceHandler({data}){
-        if(data){
+    resourceHandler({data})
+    {
+        if(data)
+        {
         //    console.log(data.fields.User__c.value);
            this.ID = data.fields.User__c.value
            console.log( "id = " + this.ID);
-      
         }
     }
 
@@ -47,22 +42,15 @@ export default class TrailBadges extends LightningElement
             console.log("ID inside wire =  " + this.ID);
             for (const key in this.users)
             {
-               
                 console.log(this.users[key].Id);
-                
                 if(this.users[key].Id == this.ID)
                 {
-                  
                     console.log('inside If');
                     this.userId = this.users[key].Id;
                     console.log(this.users[key].Badges);
                     this.count = this.users[key].Badges;
-                    
                 }    
-  
             }
-
-      
         }
 
         else if (error)
@@ -71,31 +59,24 @@ export default class TrailBadges extends LightningElement
         }
     }
 
-
-
     @wire(userBadgesInfo, { userId: '$userId' })
-    userBadgeList(data,error){
-        if(data){
+    userBadgeList(data,error)
+    {
+        if(data)
+        {
             console.log("data = " + data);
         
             const mapped = Object.entries(data);
 
             console.log(mapped)
-          
-                        
-            
-
-          
-        }else if(error){
+        }
+        else if(error)
+        {
             console.log(error);
         }
-
     }
-
 
     @wire(userBadgesInfo, { userId: '$userId' })
     badgeList
-
-
 }
 
